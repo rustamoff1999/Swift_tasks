b5969231fe46d432408dafe35bb933badf737a83
fuadrustamov999 <rustamoff1999@gmail.com>
2020-04-10 04:54:36 +0400

    Descending order

---
+++ code_wars_1.playground/Contents.swift
@@ -0,0 +1,18 @@
+func descendingOrder(of number: Int) -> Int {
+      var array : Array<Int> = []
+      var myNumber = number
+      if number != 0 {
+        while myNumber >= 1 {
+            array.append(myNumber % 10)
+            myNumber /= 10
+        }
+        array.sort(by: >)
+        var result : String = ""
+        for i in array {
+          result += String(i)
+        }
+        let myResult : Int = Int(result)!
+        return myResult
+      }
+     return number
+}
