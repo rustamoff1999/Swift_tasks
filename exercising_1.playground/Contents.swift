b47464ee8df7cd55cae73169d7f73d21e078426a
fuadrustamov999 <rustamoff1999@gmail.com>
2020-05-13 03:42:26 +0400

    Back to programming after a long vacation

---
+++ exercising_1.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//: **Exercising Int, Float, Double, truncatingRemainder(dividingBy:), Constants, Variables, typealias**
+
+let PI : Float = 3.1415926
+
+var `Int` : Int = 10
+
+typealias db = Double
+typealias fl = Float
+var num1 : fl = 12.0
+var num2 : fl = 5.0
+print("The result is =",num1.truncatingRemainder(dividingBy: num2))
